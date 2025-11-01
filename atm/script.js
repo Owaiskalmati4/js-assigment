b4c27de2df2502be3885e2372026da058835aac1
Owaiskalmati4 <kalmatiowais5@gmail.com>
2025-11-01 12:42:33 +0500

    Add files via upload

---
+++ atm/script.js
@@ -0,0 +1,10 @@
+let atmmachine=50000;
+let salary = 25000;
+
+let pin=prompt("Enter Your Pin code")
+let widraw= prompt("Enter Your Amount");
+let pincode=1234;
+
+if (pin==pincode){
+ console.log(salary-widraw)
+}
