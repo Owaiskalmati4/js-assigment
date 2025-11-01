80c8c098bbfc58054dc953e81213607b0b015f12
Owaiskalmati4 <kalmatiowais5@gmail.com>
2025-11-01 12:28:30 +0500

    file added

---
+++ array/js.js
@@ -0,0 +1,100 @@
+let stu=[]
+
+console.log(stu)
+
+
+// question no 2
+
+let student = ["Ibrahim","Talha","Hassan","Noman","hasnain","AbdulAhad"]
+
+
+console.log(student)
+
+// question no 3
+
+let string=["a","b","c","d","e"]
+
+
+console.log("String Value",string);
+
+
+// question no 4
+
+let num=[1,2,3,4,5,6,7,8]
+
+console.log("Defune the values Value",num);
+
+// question no 5 
+
+let falsevalue=["",0,null,NaN,undefined,false]
+
+let truevalue=[1,"owais"]
+
+console.log("flasvlue",falsevalue,"\n","true value",truevalue)
+
+// question no 6
+
+let mixedarray=[1,2,3,"",null,"owais","true"]
+
+console.log("the value is mixed array",mixedarray)
+
+
+// question no 7
+
+let  pakistanieducation=["ssc","hsc","bcs","bs","bcom","ms","m.phil","phd"]
+
+
+console.log("the value is pakistanieducation",pakistanieducation);
+
+
+// question no 8
+
+let names=["owais","onais","balach"]
+
+let total=[500]
+
+let score=[4.26]
+
+let score1=[15.37]
+
+let score2=[7.32]
+
+let equal=score*total/100;
+
+let equal1=score1*total/100;
+
+let equal2=score2*total/100;
+
+
+console.log("this arrya is marksheet",names,"\n",equal, "\n", equal1, "\n",equal2)
+
+
+
+// question no 9
+
+let number=[90,80,70,60,50,40,30,20,10]
+
+
+console.log(number.sort())
+
+// question no 10
+
+
+let arr = ["this","is","my","car"]
+
+console.log(arr.join());
+
+
+
+// question no 11
+
+let order = []
+
+order.push("Apple")
+order.push("bananana")
+order.push("Cherry")
+
+console.log("removed",order.pop())
+console.log("removed",order.pop())
+console.log("removed",order.pop())
+
