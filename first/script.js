f5dfcddb1f183e8d528e0b7ccb65f7e79e3febf3
Owaiskalmati4 <kalmatiowais5@gmail.com>
2025-11-01 12:48:24 +0500

    Add files via upload

---
+++ first/script.js
@@ -0,0 +1,10 @@
+alert("hello \n it's my 1st assigmenet");
+let sum1 = 12;
+let sum2 = 20;
+let sum = sum1 + sum2;
+console.log("the sum is" + sum);
+
+let username = "owais";
+let useremail = "owais4132@gmail.com";
+let password = "1234";
+console.log(username,"\n",useremail,"\n",password)
